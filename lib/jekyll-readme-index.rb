99f77c0737691b39ad1b9d264f68218a43019945
Ben Balter <ben.balter@github.com>
2018-06-08 10:13:07 -0400

    rubocop

--- lib/jekyll-readme-index.rb
+++ lib/jekyll-readme-index.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "jekyll"
 require "jekyll-readme-index/generator"
 require_relative "./jekyll/static_file_ext"
--- lib/jekyll/static_file_ext.rb
+++ lib/jekyll/static_file_ext.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Jekyll
   class StaticFile
     # Convert this static file to a Page
